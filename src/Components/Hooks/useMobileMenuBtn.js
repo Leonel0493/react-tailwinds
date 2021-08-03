c3ee5b956159f973793ccdfc28fc9d971666a326
Leonel Rivas <leonrivas04@gmail.com>
2021-08-03 14:12:34 -0600

    Funcionalidad de Mobile Menu
    Reescribiendo la funcionalidad de mobile menu

--- src/Components/Hooks/useMobileMenuBtn.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('button#mobile-menu-btn')
-const menu = document.querySelector('#mobile-menu')
-
-if (btn) {
-    btn.addEventListener("click", () => {
-        menu.classList.toggle('hidden')
-    })
-}
--- src/Components/Organisms/Menu.jsx
+++ src/Components/Organisms/Menu.jsx
@@ -1,105 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 
-const Menu = () => (
-    /* Nav goes here */
-    <nav className="mb-8 bg-gray-100">
-        <div className="max-w-7xl mx-auto px-4">
-            <div className="flex justify-between">
-                <div className="flex space-x-4">
-                    <div id="logo">
-                        <Link to="/" className="flex items-center py-5 px-3 font-bold text-yellow-500 hover:text-yellow-600">
-                            <svg className="h-6 w-6 mr-1 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
-                            </svg>
-                            <span>Lion Dev</span>
-                        </Link>
+const Menu = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    return (
+        /* Nav goes here */
+        <nav className="bg-gray-100">
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="flex justify-between">
+                    <div className="flex space-x-4">
+                        <div id="logo">
+                            <Link to="/" className="flex items-center py-5 px-3 font-bold text-yellow-500 hover:text-yellow-600">
+                                <svg className="h-6 w-6 mr-1 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+                                </svg>
+                                <span>Lion Dev</span>
+                            </Link>
+                        </div>
+
+                        <div id="primary_nav" className="hidden md:flex items-center space-x-1">
+                            <NavLink to='/' exact activeClassName="active" className="flex py-5 px-3 text-gray-600 hover:text-gray-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                </svg>
+                                <span>Inicio</span>
+                            </NavLink>
+                            <NavLink to='/Administracion' activeClassName="active" className="flex py-5 px-3 text-gray-600 hover:text-gray-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                                </svg>
+                                <span>Administracion</span>
+                            </NavLink>
+                            <NavLink to='/Formulario' activeClassName="active" className="flex py-5 px-3 text-gray-600 hover:text-gray-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
+                                </svg>
+                                <span>Formulario</span>
+                            </NavLink>
+                            <NavLink to='/Perfil' activeClassName="active" className="flex py-5 px-3 text-gray-600 hover:text-gray-800">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+                                </svg>
+                                <span>Perfil</span>
+                            </NavLink>
+                        </div>
                     </div>
 
-                    <div id="primary_nav" className="hidden md:flex items-center space-x-1">
-                        <NavLink className="flex py-5 px-3 text-gray-600 hover:text-gray-800" to='/' exact>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                            </svg>
-                            <span>Home</span>
-                        </NavLink>
-                        <NavLink className="flex py-5 px-3 text-gray-600 hover:text-gray-800" to='/Url1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                            </svg>
-                            <span>Something 1</span>
-                        </NavLink>
-                        <NavLink className="flex py-5 px-3 text-gray-600 hover:text-gray-800" to='/Url1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
-                            </svg>
-                            <span>Something 2</span>
-                        </NavLink>
-                        <NavLink className="flex py-5 px-3 text-gray-600 hover:text-gray-800" to='/Url1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                            </svg>
-                            <span>Something 3</span>
-                        </NavLink>
-                        <NavLink className="flex py-5 px-3 text-gray-600 hover:text-gray-800" to='/Url1'>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                            </svg>
-                            <span>Something 4</span>
-                        </NavLink>
+                    <div id="secondary_nav" className="hidden md:flex items-center space-x-1">
+                        <Link className="py-5 px-3" to='/login'>Login</Link>
+                        <Link className="py-2 px-3 bg-green-600 hover:bg-green-400 text-white hover:text-green-700 rounded transition duration-300" to='/signup'>Signup</Link>
                     </div>
-                </div>
 
-                <div id="secondary_nav" className="hidden md:flex items-center space-x-1">
-                    <Link className="py-5 px-3" to='/login'>Login</Link>
-                    <Link className="py-2 px-3 bg-green-600 hover:bg-green-400 text-white hover:text-green-700 rounded transition duration-300" to='/signup'>Signup</Link>
-                </div>
-
-                <div id="mobile" className="md:hidden flex items-center">
-                    <button id="mobile-menu-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                    </button>
+                    <div id="mobile" className="md:hidden flex items-center" onClick={handleClick}>
+                        <button id="mobile-menu-btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div id="mobile-menu" className="hidden md:hidden">
-            <NavLink className="flex items-center py-2 px-4 text-sm hover:bg-gray-200" to='/' exact>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                </svg>
-                <span>Home</span>
-            </NavLink>
-            <NavLink className="flex py-2 px-4 text-sm hover:bg-gray-200" to='/Url1'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-                <span>Something 1</span>
-            </NavLink>
-            <NavLink className="flex py-2 px-4 text-sm hover:bg-gray-200" to='/Url1'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
-                </svg>
-                <span>Something 2</span>
-            </NavLink>
-            <NavLink className="flex py-2 px-4 text-sm hover:bg-gray-200" to='/Url1'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                </svg>
-                <span>Something 3</span>
-            </NavLink>
-            <NavLink className="flex py-2 px-4 text-sm hover:bg-gray-200" to='/Url1'>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>
-                <span>Something 4</span>
-            </NavLink>
-        </div>
-    </nav>
-)
+            <div id="mobile-menu" className={ click ? "md:hidden" : " hidden md:hidden" }>
+                <NavLink to='/' exact activeClassName="mobile-active" className="flex items-center py-2 px-4 text-sm hover:bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    <span>Home</span>
+                </NavLink>
+                <NavLink to='/Administracion' activeClassName="mobile-active" className="flex py-2 px-4 text-sm hover:bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    <span>Something 1</span>
+                </NavLink>
+                <NavLink to='/Formulario' activeClassName="mobile-active" className="flex py-2 px-4 text-sm hover:bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
+                    </svg>
+                    <span>Something 2</span>
+                </NavLink>
+                <NavLink to='/Perfil' activeClassName="mobile-active" className="flex py-2 px-4 text-sm hover:bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+                    </svg>
+                    <span>Something 3</span>
+                </NavLink>
+            </div>
+        </nav>
+    )
+}
 
 export default Menu
